0675b7f8ecab3fd93d9968122e5a4936f79af608
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-09 23:34:50 +0530

    added dispath and navigate the user in the hook state

---
+++ src/components/Body.js
@@ -0,0 +1,47 @@
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter, RouterProvider, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import {addUser, removeUser} from "../utils/userSlice";
+
+const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const appRouter = createBrowserRouter([
+        {
+            path:"/",
+            element: <Login/>
+        },
+        {
+            path:"/browse",
+            element: <Browse/>
+        }
+    ]);
+
+
+
+    useEffect(()=> {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                
+                const {uid, email, displayName}= user;
+                dispatch(addUser({uid: uid, email: email, displayName: displayName}));
+                navigate("/browse")
+                
+            } else {
+                dispatch(removeUser());
+            }
+            });
+    },[]);
+
+
+  return (
+    <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+  )
+}
+
+export default Body
