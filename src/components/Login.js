c82e769c7d6d290773e5a50488744e80a796d6c9
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-10 23:55:33 +0530

    css changes for the background image div

--- src/components/Login.js
+++ src/components/Login.js
@@ -48,8 +48,8 @@ const Login = () => {
   return (
     <div>
         <Header/>
-        <div>
-            <img className="absolute" src='https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg' alt='background'/>
+        <div className="hidden sm:block">
+            <img className="absolute w-full h-full object-cover" src='https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg' alt='background'/>
         </div>
         <form onSubmit={(e) => e.preventDefault()} className='absolute inset-0 mx-auto my-20 p-6 sm:p-10 md:p-12 w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 max-w-[500px] bg-black text-white rounded-lg bg-opacity-80'>
             <h1 className='font-bold text-2xl sm:text-3xl py-4'>
@@ -59,10 +59,10 @@ const Login = () => {
             <input ref={email} type='text' placeholder='Email Address' className='py-2 px-3 m-2 w-full bg-gray-700 rounded'/>
             <input ref={password} type='password' placeholder='Password' className='py-2 px-3 m-2 w-full bg-gray-700 rounded'/>
             {errorMessage && <p className='text-red-500 text-sm mx-2'>{errorMessage}</p>}
-            <button className='py-2 m-2 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>
+            <button className='py-2 m-2 bg-red-700 w-full rounded-lg hover:bg-red-600 transition' onClick={handleButtonClick}>
                 {isSignInForm ? "Sign In" : "Sign Up"}
             </button>
-            <p  className="cursor-pointer" onClick={toggleSignInForm}>{isSignInForm ? "New to the Netflix? Sign Up" : "Already Registered. Sign In"}</p>
+            <p  className="ccursor-pointer text-sm text-gray-300 hover:underline mt-2 text-center" onClick={toggleSignInForm}>{isSignInForm ? "New to the Netflix? Sign Up" : "Already Registered. Sign In"}</p>
         </form>
     </div>
   )
