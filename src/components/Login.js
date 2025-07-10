d47f07c3a07136e15c6a4a29648ba98cc40f9209
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-10 23:37:48 +0530

    change in the css of the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -51,14 +51,14 @@ const Login = () => {
         <div>
             <img className="absolute" src='https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg' alt='background'/>
         </div>
-        <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
-            <h1 className='font-bold text-3xl py-4'>
+        <form onSubmit={(e) => e.preventDefault()} className='absolute inset-0 mx-auto my-20 p-6 sm:p-10 md:p-12 w-11/12 sm:w-8/12 md:w-6/12 lg:w-4/12 xl:w-3/12 max-w-[500px] bg-black text-white rounded-lg bg-opacity-80'>
+            <h1 className='font-bold text-2xl sm:text-3xl py-4'>
                 {isSignInForm ? "Sign In" : "Sign Up"}
             </h1>
-            {!isSignInForm && (<input type='text' placeholder='Full Name' className='py-2 m-2 w-full bg-gray-700'/>)}
-            <input ref={email} type='text' placeholder='Email Address' className='py-2 m-2 w-full bg-gray-700'/>
-            <input ref={password} type='password' placeholder='Password' className='py-2 m-2 w-full bg-gray-700'/>
-            {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+            {!isSignInForm && (<input type='text' placeholder='Full Name' className='py-2 px-3 m-2 w-full bg-gray-700 rounded'/>)}
+            <input ref={email} type='text' placeholder='Email Address' className='py-2 px-3 m-2 w-full bg-gray-700 rounded'/>
+            <input ref={password} type='password' placeholder='Password' className='py-2 px-3 m-2 w-full bg-gray-700 rounded'/>
+            {errorMessage && <p className='text-red-500 text-sm mx-2'>{errorMessage}</p>}
             <button className='py-2 m-2 bg-red-700 w-full rounded-lg' onClick={handleButtonClick}>
                 {isSignInForm ? "Sign In" : "Sign Up"}
             </button>
