5f3b72b3e1d4bca72b0be2ce6b59a7fddfbe4d91
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-16 14:06:39 +0530

    created a user name while signIn and signOut , show the user icon only when user logs In

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import { signOut } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      navigate("/")
+    }).catch((error) => {
+       navigate("/error");
+    });
+  }
+
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+    <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
         <img className = "w-44" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+        {user && <div className='flex p-5'>
+          <img 
+            className='w-8 h-8'
+            alt='userIcon' 
+            src='https://occ-0-2483-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229'
+          />
+          <button onClick={handleSignOut} className='font-bold text-blue-900'>Sign Out</button>
+        </div>}
     </div>
   )
 }
