a83ef9b1349b4d4a70b96ed548c9effee1cfe744
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-25 23:33:16 +0530

    removed the hard coded logo and import that logo from the constants

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from '../utils/userSlice';
 import { useEffect } from 'react';
+import { LOGO } from '../utils/constant';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -17,7 +18,7 @@ const Header = () => {
   }
 
    useEffect(()=> {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 
                 const {uid, email, displayName}= user;
@@ -28,12 +29,14 @@ const Header = () => {
                 navigate("/");
             }
             });
+            return () => unsubscribe();
+
     },[]);
 
 
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between'>
-        <img className = "w-44" src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo"/>
+        <img className = "w-44" src={LOGO} alt="logo"/>
         {user && <div className='flex p-5'>
           <img 
             className='w-8 h-8'
