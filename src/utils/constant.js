59b39e6dfbebd13613c8ccefcf3d1f901985f6ef
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-25 23:38:25 +0530

    created new component for constant url

---
+++ src/utils/constant.js
@@ -0,0 +1,12 @@
+export const LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-07-01/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_AVATAR = "https://occ-0-2483-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+
+export const API_OPTION = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzBkMWQ2MGQ5ZDQ3MTQzNzczYmYyNGQzNDU1N2EwYiIsIm5iZiI6MTc1MjY1NDQ0Ny43OTQsInN1YiI6IjY4Nzc2MjZmNjJlM2E0NDFlYTc3OThiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BHbg1NU7JTNYGnkwsbiV9aCVIyLFlmt-2PWWFIz_Vr4'
+  }
+};
