26395a5e209ffee67de7c58242a8478c5a9ab29a
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-27 22:58:08 +0530

    removed all the unwanted imports

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,7 @@
 import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter, RouterProvider} from "react-router-dom";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
 import { useDispatch } from "react-redux";
-import {addUser, removeUser} from "../utils/userSlice";
-import { auth } from '../utils/firebase';
 
 const Body = () => {
     const dispatch = useDispatch();
