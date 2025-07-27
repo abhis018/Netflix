5a0b8a6a3c7f7aa983e98d5c73dc3a78ebcef569
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-27 22:59:47 +0530

    added new component of movie cards

---
+++ src/components/MovieCards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IMG_CDN_URL } from '../utils/constant'
+
+const MovieCards = ({posterPath}) => {
+  return (
+    <div className='w-44 pr-2 m-1'>
+        <img alt="Movie-Img" src={IMG_CDN_URL + posterPath}/>
+    </div>
+  )
+}
+
+export default MovieCards
