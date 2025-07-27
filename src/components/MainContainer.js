0addf3bf5bef047f9110b807478b2ac2b4809f57
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-27 22:59:27 +0530

    nothing changes

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,12 +7,11 @@ const MainContainer = () => {
     if(!movies) return;
 
     const mainMovie = movies[0];
-    console.log(mainMovie);
     const {original_title, overview, id} = mainMovie;
 
 
   return (
-    <div>
+    <div className=''>
         <VideoTitle title={original_title} overview = {overview}/>
         <VideoBackground movieId={id}/>
     </div>
