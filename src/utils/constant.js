01d5edfc2ef3fc555d1cf71e61b625ae4159dbf9
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-27 23:02:34 +0530

    exporting the image url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -10,3 +10,6 @@ export const API_OPTION = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzBkMWQ2MGQ5ZDQ3MTQzNzczYmYyNGQzNDU1N2EwYiIsIm5iZiI6MTc1MjY1NDQ0Ny43OTQsInN1YiI6IjY4Nzc2MjZmNjJlM2E0NDFlYTc3OThiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BHbg1NU7JTNYGnkwsbiV9aCVIyLFlmt-2PWWFIz_Vr4'
   }
 };
+
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
