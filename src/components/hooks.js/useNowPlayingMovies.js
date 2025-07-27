44f6650cd7b33188a848b56ac9d4497b4e10f5e8
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-27 23:02:14 +0530

    debugging the fetch call

--- src/components/hooks.js/useNowPlayingMovies.js
+++ src/components/hooks.js/useNowPlayingMovies.js
@@ -10,8 +10,8 @@ const useNowPlayingMovies = () => {
   const getNowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTION);
     const json = await data.json();
-    console.log(json.results );
     dispatch(addNowPlayingMovies(json.results));
+    console.log(json)
   }
 
   useEffect(() => {
