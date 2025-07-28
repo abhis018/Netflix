5d5d187fb4780d104502e8564a99921a2a17ae8b
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-28 23:30:28 +0530

    import hooks for popular trending and upcoming movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,11 +1,17 @@
 import Header from './Header';
 import useNowPlayingMovies from './hooks.js/useNowPlayingMovies';
+import usePopularMovies from './hooks.js/usePopularMovies';
+import useTrendingMovies from './hooks.js/useTrendingMovies';
+import useUpcomingMovies from './hooks.js/useUpcomingMovies';
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer"
 
 
 const Browse = () => {
  useNowPlayingMovies();
+ usePopularMovies();
+ useTrendingMovies();
+ useUpcomingMovies();
 
   return (
     <div className='bg-[#141414] text-white p-4'>
