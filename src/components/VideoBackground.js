7b58cd90a69485ed8d83d8e3b50afa4974e0ade3
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-28 23:33:07 +0530

    change the styling of the iframe

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -6,12 +6,12 @@ const VideoBackground = ({movieId}) => {
     useMovieTrailer(movieId);
      if (!trailerVideo) return null;
   return (
-    <div className="top-0 left-0 w-[100%] h-screen z-0">
+    <div className="absolute top-0 left-0 w-full h-screen z-0">
       <iframe
         className="w-full h-full"
-        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0`}
+        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo.key}&modestbranding=1&showinfo=0&rel=0`}
         title="YouTube video player"
-        allow="autoplay; encrypted-media"
+        allow="autoplay; fullscreen"
         allowFullScreen
         referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
