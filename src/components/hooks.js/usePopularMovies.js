2328372d335313b0aaa9b78204ce24674a7af0f7
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-28 23:34:10 +0530

    created a hooks for the popular movies

---
+++ src/components/hooks.js/usePopularMovies.js
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { API_OPTION } from "../../utils/constant";
+import { addPopularMovies } from "../../utils/moviesSlice";
+import { useEffect } from "react";
+
+
+const usePopularMovies = () => {
+    const dispatch = useDispatch();
+
+  const getPopularMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1', API_OPTION);
+    const json = await data.json();
+    dispatch(addPopularMovies(json.results));
+    console.log(json)
+  }
+
+  useEffect(() => {
+    getPopularMovies();
+  },[]);
+}
+
+
+export default usePopularMovies;
