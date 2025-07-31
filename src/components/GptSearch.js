f6632a697932fea2805ffe6d73f6cd81e653ae50
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-07-31 22:56:06 +0530

    added a gpt search bar and movie suggestion in the gpt search component

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import GptSearchBar from './GptSearchBar'
+import GptMovieSuggestion from './GptMovieSuggestion'
 
 const GptSearch = () => {
   return (
-    <div>GptSearch</div>
+    <div>
+      <GptSearchBar/>
+      <GptMovieSuggestion/>
+    </div>
   )
 }
 
