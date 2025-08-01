d1c98ec66e23ba495959856fe149290d44d5fb8b
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-01 22:14:09 +0530

    check posterpath is present or not

--- src/components/MovieCards.js
+++ src/components/MovieCards.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { IMG_CDN_URL } from '../utils/constant'
 
 const MovieCards = ({posterPath}) => {
+  if(!posterPath) return null;
   return (
     <div className='w-44 pr-2 m-1'>
         <img alt="Movie-Img" src={IMG_CDN_URL + posterPath}/>
