0fa9710f871f7b8399507188ddb73dc20c38c815
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-01 22:12:15 +0530

    added movielist card in the movie suggestion

--- src/components/GptMovieSuggestion.js
+++ src/components/GptMovieSuggestion.js
@@ -1,12 +1,12 @@
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList"
 
 const GptMovieSuggestion = () => {
   const { movieNames, movieResults } = useSelector(store => store.gpt);
   if(!movieNames) return null
   return (
     <div className="p-4 m-4 bg-black">
-      
-
+      {movieNames.map((movieName, index) => <MovieList key={movieName} title={movieName} movies={movieResults[index]}/>)}
     </div>
   )
 }
