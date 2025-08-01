b1d35708d3ca2755cb94b5b0376ed4be5193e3c7
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-01 22:13:38 +0530

    clear the styling section

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,14 +6,14 @@ const MainContainer = () => {
     const movies = useSelector(store => store.movies?.nowPlayingMovies);
     if(!movies) return;
 
-    const mainMovie = movies[0];
+    const mainMovie = movies[1];
     const {original_title, overview, id} = mainMovie;
 
 
   return (
     <div className='relative w-full h-screen'>
         <VideoBackground movieId={id}/>
-        <div className="absolute inset-0 flex flex-col justify-center px-8 sm:px-12 lg:px-20 z-20 text-white bg-gradient-to-r from-black/80 via-transparent to-transparent">
+      <div>
         <VideoTitle title={original_title} overview={overview} movieId={id}/>
       </div>
     </div>
