83fef4459ce9cb8491f76f460820bdfd20ee91b1
abhis018 <76279317+abhis018@users.noreply.github.com>
2025-08-01 22:15:41 +0530

    adding store for the section

--- src/components/hooks.js/useNowPlayingMovies.js
+++ src/components/hooks.js/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTION } from "../../utils/constant";
 import { addNowPlayingMovies } from "../../utils/moviesSlice";
 import { useEffect } from "react";
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
     const dispatch = useDispatch();
 
+    const nowPlayingMovies = useSelector(store => store.movies.nowPlayingMovies);
+
   const getNowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTION);
     const json = await data.json();
@@ -14,7 +16,7 @@ const useNowPlayingMovies = () => {
   }
 
   useEffect(() => {
-    getNowPlayingMovies();
+    if(!nowPlayingMovies) getNowPlayingMovies();
   },[]);
 }
 
--- src/components/hooks.js/usePopularMovies.js
+++ src/components/hooks.js/usePopularMovies.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTION } from "../../utils/constant";
 import { addPopularMovies } from "../../utils/moviesSlice";
 import { useEffect } from "react";
@@ -7,6 +7,8 @@ import { useEffect } from "react";
 const usePopularMovies = () => {
     const dispatch = useDispatch();
 
+    const popularMovies = useSelector((store) => store.movies.popularMovies)
+
   const getPopularMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1', API_OPTION);
     const json = await data.json();
@@ -14,7 +16,7 @@ const usePopularMovies = () => {
   }
 
   useEffect(() => {
-    getPopularMovies();
+    !popularMovies && getPopularMovies();
   },[]);
 }
 
